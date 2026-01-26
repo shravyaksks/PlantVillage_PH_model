e853e7c1c847b102598d66ef8b8dd8632baf2d36
Shravya <shravyakshankar@gmail.com>
2026-01-26 10:08:47 +0530

    Use environment variable for server port
    Updated the app to use environment variable for port.

--- app_ph_model_ai.py
+++ app_ph_model_ai.py
@@ -54,6 +54,9 @@ def index():
 
 # 🔹 Add this at the very bottom
 if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=False, host="0.0.0.0", port=port)
+
    # app.run(debug=True)
-    app.run(debug=False, host="0.0.0.0", port=5000)
+   # app.run(debug=False, host="0.0.0.0", port=5000)
 
